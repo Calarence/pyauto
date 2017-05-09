c7d165b1953e00dbe8d4133c0259ef87036b8de8
Calarence <749826557@qq.com>
2017-05-09 15:34:07 +0800

    modify todo

--- 第一章/procsmem.py
+++ 第一章/procsmem.py
@@ -1,4 +1,7 @@
 import psutil
+import sys
+if not (psutil.LINUX or psutil.WINDOWS or psutil.OSX):
+    sys.exit("platform not supported")
 
 
 def bytes2human(n):
@@ -13,15 +16,52 @@ def bytes2human(n):
     return "%sB" % n
 
 
-def print_info():
-    for pid in psutil.pids():
-        process = psutil.Process(pid)
-        USS = bytes2human(process.memory_full_info()[7])
-        PSS = bytes2human(process.memory_full_info()[8])
-        Swap = bytes2human(process.memory_full_info()[9])
-        RSS = bytes2human(process.memory_full_info()[0])
-        print("%s %s %s %s %s %s %s" % (str(pid), process.name(),
-                                        process.cmdline(), USS, PSS, Swap, RSS))
+def main():
+    ad_pids = []
+    procs = []
+    for process in psutil.process_iter():
+        try:
+            memoInfo = process.memory_full_info()
+            proInfo = process.as_dict(attrs=["cmdline", "username"])
+        except psutil.AccessDenied:
+            ad_pids.append(process.pid)
+        except psutil.NoSuchProcess:
+            pass
+        else:
+            process._uss = memoInfo.uss
+            process._rss = memoInfo.rss
+            if not process._uss:
+                continue
+            """ 
+            OSX and windows do not have pss and swap metric
+            """
+            process._pss = getattr(memoInfo, 'pss', '')
+            process._swap = getattr(memoInfo, 'swap', '')
+            process._info = proInfo
+            procs.append(process)
+    procs.sort(key=lambda process: process._uss)
+    templ = "%-7s %-7s %-30s %7s %7s %7s %7s"
+    print(templ % ("PID", "User", "Cmdline", "USS", "PSS", "Swap", "RSS"))
+    print("=" * 78)
+    for process in procs:
+        line = templ % (
+            process.pid,
+            process._info["username"][:7],
+            " ".join(process._info['cmdline'])[:30],
+            bytes2human(process._uss),
+            bytes2human(process._pss) if process._pss else "",
+            bytes2human(process._swap) if process._swap else "",
+            bytes2human(process._rss) if process._rss else ""
+        )
+        print(line)
+    if ad_pids:
+        print("Warining: access denied for % pids" % len(ad_pids))
+
+        # USS = bytes2human(process.memory_full_info()[7])
+        # PSS = bytes2human(process.memory_full_info()[8])
+        # Swap = bytes2human(process.memory_full_info()[9])
+        # RSS = bytes2human(process.memory_full_info()[0])
+        # print("%s %s %s %s %s %s %s" % (str(pid), process.name(),
+        # process.cmdline(), USS, PSS, Swap, RSS))
 if __name__ == '__main__':
-    print("pid    name     cmdline    USS     PSS    Swap     RSS")
-    print_info()
+    sys.exit(main())
